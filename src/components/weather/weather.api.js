a7dd2b1d6b22ffd42fd6088fae97efab28bc1d0b
galzerk Manu <50106708+ManuGalzerk@users.noreply.github.com>
2025-03-19 09:19:27 +0100

    Update weather.api.js

--- src/components/weather/weather.api.js
+++ src/components/weather/weather.api.js
@@ -1,25 +1,20 @@
 class WeatherForecastClient {
-  constructor(location) {
-    this.appId = "50a34e070dd5c09a99554b57ab7ea7e2";
-    this.url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(location)}&units=metric&appid=${
-      this.appId
-    }`;
+  constructor(lat, lon) {
+    this.appId = "50a34e070dd5c09a99554b57ab7ea7e2"; // Sostituisci con la tua API Key
+    this.url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${this.appId}`;
   }
 
   async getWeather() {
-    return await fetch(this.url)
+    return fetch(this.url)
       .then((res) => res.json())
-      .then((json) => JSON.stringify(json))
-      .then((json) => JSON.parse(json))
-      .then((data) => {
-        const temperature = Math.round(data.main.temp);
-        const condition = data.weather[0].main.toLowerCase();
-
-        return {
-          temperature,
-          condition,
-        };
-      })
-      .catch((err) => console.warn("Weather API returned an error:", err));
+      .then((data) => ({
+        temperature: Math.round(data.main.temp),
+        condition: data.weather[0].main.toLowerCase(),
+      }))
+      .catch((err) => console.warn("Errore API meteo:", err));
   }
 }
+
+// USO: Ottieni il meteo per la tua posizione
+const client = new WeatherForecastClient(43.370096, 13.579037);
+client.getWeather().then((data) => console.log(data));
